ee726f4c4961eb499703aed961d7d587acaffe04
OskOkr <120591511+OskOkr@users.noreply.github.com>
2023-06-03 15:23:35 +0200

    Poprawki cieni, usunięcie chodzenia po wodzie

--- pakiety/wczytywanie.go
+++ pakiety/wczytywanie.go
@@ -8,6 +8,8 @@ import (
 	// "github.com/fstanis/screenresolution"
 	"github.com/veandco/go-sdl2/sdl" 
 	"log"
+	"math/rand"
+	"time"
 )
 
 // Funkcja wczytująca dane z pliku do tablicy dwuwymiarowej
@@ -129,8 +131,8 @@ func Rysuj (wysokosc int, szerokosc int, jednostki [][]int, swiat [][]int, windo
 		return nil
 	case 1://JEDNOSTKA(MAIN)
 		color = sdl.Color{R: 88, G: 65, B: 18, A: 220}
-	case 2://JEDNOSTKA(SOJUSZNIK)
-		color = sdl.Color{R: 102, G: 255, B: 102, A: 220}
+	case 2://JEDNOSTKA(WROG_BYLE_POLE)
+		color = sdl.Color{R: 51, G: 0, B: 0, A: 220}
 	case 3://JEDNOSTKA(WROG)
 		color = sdl.Color{R: 236, G: 0, B: 0, A: 220} 
 	}
@@ -151,35 +153,12 @@ func Rysuj (wysokosc int, szerokosc int, jednostki [][]int, swiat [][]int, windo
 
 }
 
-func czypuste (jednostki [][]int, i int, j int, kierunek rune) (bool) { //i, j kordynaty
-	switch kierunek {
-	case 'w':
-		if jednostki[i-1][j]==0 || jednostki[i-1][j]==3 {
-			return true
-		} else {
-			return false
-		  }
-	case 's':
-		if jednostki[i+1][j]==0 || jednostki[i+1][j]==3 {
-			return true
-		} else {
-			return false
-		  }
-	case 'd':
-		if jednostki[i][j+1]==0 || jednostki[i][j+1]==3 {
-			return true
-		} else {
-			return false
-		  }
-	case 'a':
-		if jednostki[i][j-1]==0 || jednostki[i][j-1]==3 {
-			return true
-		} else {
-			return false
-		  }
-	}
-	return false
-
+func czypuste (jednostki [][]int, i int, j int, kto int, swiat [][]int) (bool) { //i, j kordynaty
+	if (jednostki[i][j]==0 || (jednostki[i][j]==3 && kto == 1) || jednostki[i][j]==2) && (swiat[i][j] != 0 && swiat[i][j] != 20) {
+		return true
+	} else {
+		return false
+	   }
 }
 
 func znajdzgracza (jednostki [][]int) (int, int) {
@@ -199,28 +178,28 @@ func PrzemiescGracza (znak rune, jedn *[][]int, window *sdl.Window, swiat [][]in
 
     switch znak {
     case 's'://S
-        if czypuste(jednostki, i, j, znak) {
+        if czypuste(jednostki, i+1, j, 1, swiat) {
             jednostki[i][j]=0
             jednostki[i+1][j]=1
 			Rysuj(i, j, jednostki, swiat, window)
 			Rysuj(i+1, j, jednostki, swiat, window)
         }
     case 'w': //W
-        if czypuste(jednostki, i, j, znak) {
+        if czypuste(jednostki, i-1, j, 1, swiat) {
             jednostki[i][j]=0
             jednostki[i-1][j]=1
 			Rysuj(i, j, jednostki, swiat, window)
 			Rysuj(i-1, j, jednostki, swiat, window)
         }
     case 'a': //A
-        if czypuste(jednostki, i, j, znak) {
+        if czypuste(jednostki, i, j-1, 1, swiat) {
             jednostki[i][j]=0
             jednostki[i][j-1]=1
 			Rysuj(i, j, jednostki, swiat, window)
 			Rysuj(i, j-1, jednostki, swiat, window)
          }
     case 'd': //D
-        if czypuste(jednostki, i, j, znak) {
+        if czypuste(jednostki, i, j+1, 1, swiat) {
             jednostki[i][j]=0
             jednostki[i][j+1]=1
 			Rysuj(i, j, jednostki, swiat, window)
@@ -237,11 +216,126 @@ func wzrok (i int, j int, jednostki [][]int, swi *[][]int, window *sdl.Window) (
 	}
 }
 
-func Widocznosc (jednostki[][]int, swiat *[][]int, window *sdl.Window) ()  {
+func Widocznosc (jednostki[][]int, swi *[][]int, window *sdl.Window) ()  {
 	i, j := znajdzgracza(jednostki)
+	// swiat := *swi
+	// wzrok(i, j, jednostki, swiat, window)
+	// wzrok(i+1, j, jednostki, swiat, window)
+	// wzrok(i, j+1, jednostki, swiat, window)
+	// wzrok(i-1, j, jednostki, swiat, window)
+	// wzrok(i, j-1, jednostki, swiat, window)
+	wzrok3(i, j, jednostki, swi, window, 2)
+}
+
+func wzrok3 (i int, j int, jednostki [][]int, swiat *[][]int, window *sdl.Window, zasieg int) () {
+	swi := *swiat
 	wzrok(i, j, jednostki, swiat, window)
-	wzrok(i+1, j, jednostki, swiat, window)
-	wzrok(i, j+1, jednostki, swiat, window)
-	wzrok(i-1, j, jednostki, swiat, window)
-	wzrok(i, j-1, jednostki, swiat, window)
+	
+	if zasieg >= 0 && i>0 && j>0 && i<39 && j<39 && (zasieg==2 || (swi[i][j]!=2 && swi[i][j]!=22)) {
+		wzrok3(i-1, j, jednostki, swiat, window, zasieg-1)
+		wzrok3(i+1, j, jednostki, swiat, window, zasieg-1)
+		wzrok3(i, j-1, jednostki, swiat, window, zasieg-1)
+		wzrok3(i, j+1, jednostki, swiat, window, zasieg-1)
+	}
+}
+
+func SzukajWroga (i int, j int, jednostki [][]int) (int, int) {
+	for a:=i; a<len(jednostki); a++ {
+		for b:=j; b<len(jednostki[a]); b++ {
+			if jednostki[a][b]==3 {
+				return a, b
+			}
+		}
+	}
+	return -1, -1
+}
+
+func PrzemiescWroga (jedn *[][]int, swiat [][]int, window *sdl.Window) () {
+	jednostki := *jedn
+	// i, j := 0, 0
+	
+	for i, _ := range jednostki {
+		for j, _ := range jednostki {
+				if jednostki[i][j]==3 {
+					defer RuchWroga(i, j, jedn, swiat, window)	
+				}
+		}
+	}
+	
+			// i, j = SzukajWroga(i, j, jednostki)
+}
+
+func RuchWroga (i int, j int, jedn *[][]int, swiat [][]int, window *sdl.Window) () {
+	jednostki := *jedn
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	random := r1.Intn(4) + 1
+	
+	switch random {
+	case 1:
+		if czypuste(jednostki, i-1, j, 3, swiat) {
+			jednostki[i][j] = 2
+			err := Rysuj(i, j, jednostki, swiat, window)
+			if err != nil {
+				panic(err)
+			}
+			jednostki[i-1][j]=3
+			err = Rysuj(i-1, j, jednostki, swiat, window)
+			if err != nil {
+				panic(err)
+			}
+		}
+	case 2:
+		if czypuste(jednostki, i+1, j, 3, swiat) {
+			jednostki[i][j] = 2
+			err := Rysuj(i, j, jednostki, swiat, window)
+			if err != nil {
+				panic(err)
+			}
+			jednostki[i+1][j]=3
+			err = Rysuj(i+1, j, jednostki, swiat, window)
+			if err != nil {
+				panic(err)
+			}
+		}
+	case 3:
+		if czypuste(jednostki, i, j-1, 3, swiat) {
+			jednostki[i][j] = 2
+			err := Rysuj(i, j, jednostki, swiat, window)
+			if err != nil {
+				panic(err)
+			}
+			jednostki[i][j-1]=3
+			err = Rysuj(i, j-1, jednostki, swiat, window)
+			if err != nil {
+				panic(err)
+			}
+		}
+	case 4:
+		if czypuste(jednostki, i, j+1, 3, swiat) {
+			jednostki[i][j] = 2
+			err := Rysuj(i, j, jednostki, swiat, window)
+			if err != nil {
+				panic(err)
+			}
+			jednostki[i][j+1]=3
+			err = Rysuj(i, j+1, jednostki, swiat, window)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+}
+
+func UsunSlady (jedn *[][]int, swiat [][]int, window *sdl.Window) () {
+	jednostki := *jedn
+	
+	for i, _ := range jednostki {
+		for j, _ := range jednostki {
+			if jednostki[i][j]==2 {
+				jednostki[i][j]=0
+				Rysuj(i, j, jednostki, swiat, window)
+			}
+		}
+	}
 }
